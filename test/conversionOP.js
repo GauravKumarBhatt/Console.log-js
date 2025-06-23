1454ab0526a8424afcf7a25e0ff54c45de2f5f98
GauravKumarBhatt <bhatt@Grv>
2025-06-23 19:27:55 +0530

    commit coversionOP

--- test/conversionOP.js
+++ test/conversionOP.js
@@ -23,5 +23,55 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *****************************operations****************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+// console.log(value)
+
+// console.log(value + 2) // addition
+// console.log(value - 2) // subtraction
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Gaurav"
+let str2 = "Bhatt"
+let str3 = str1 + str2
+// let str4 = str1 + " " + str2
+// console.log(str3)
+// console.log(str4)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 10
+console.log(num1, num2, num3);
+
+let gameScore = 100
+++gameScore;
+console.log(gameScore);
+
+// console.log("confusion with + operator");
+// console.log("1" + 2 + 2 + "2");
+// console.log(1 + 2 + 2 + "2");
+// console.log(1 + 2 + "2" + 2 * 2);
+// console.log(1 + "2" + 2 + 2);
+// console.log("1" + "2" + 2 + 2);
