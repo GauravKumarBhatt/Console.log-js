8722232edd4d6b0285e1197ba062a4995c43f2ce
GauravKumarBhatt <bhatt@Grv>
2025-07-02 23:44:47 +0530

    project commit to git

---
+++ Basic_Project/Digital_clock/index.js
@@ -0,0 +1,10 @@
+// js code for digital clock
+
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock')
+
+setInterval(function () {
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
